2c3a3ad6dc82076f7e6cb47e44ba006ac227d5f4
Seolyu <seolyupjt@gmail.com>
2022-11-16 19:55:39 +0900

    three.js 시작하기 - 자바스크립트 module 기본

---
+++ start/module_basic/hello.js
@@ -0,0 +1,7 @@
+export default function hello1() {
+  console.log("안녕1");
+}
+
+export function hello2() {
+  console.log("안녕2");
+}
---
+++ start/module_basic/main.js
@@ -0,0 +1,7 @@
+// import { hello1, hello2 } from "./hello.js";
+// import * as hello from "./hello.js";
+import hello1 from "./hello.js";
+
+// hello.hello1();
+// hello.hello2();
+hello1();
