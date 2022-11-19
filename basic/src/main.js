bf148cf6e5d9e9c1e47d5dad38b0348862dbbbc4
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2022-11-19 23:00:10 +0900

    three.js의 기본 요소 익히기 - 기본 장면 만들기 Camera

--- basic/src/main.js
+++ basic/src/main.js
@@ -8,7 +8,21 @@ console.log(THREE);
 // console.log(renderer.domElement);
 // document.body.appendChild(renderer.domElement);
 
+// html에서 캔버스 가져와서 사용
 const canvas = document.querySelector("#three-canvas");
 // const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75, // 시야각
+  window.innerWidth / window.innerHeight,
+  0.1, // near
+  1000 // far
+);
+camera.position.z = 5;
+scene.add(camera);
