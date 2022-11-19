950a5f2a518985b83bcd97b014dd3906aef0be75
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2022-11-20 00:22:11 +0900

    three.js의 기본 요소 익히기 - 기본 장면 만들기 Mesh

--- basic/src/main.js
+++ basic/src/main.js
@@ -11,7 +11,10 @@ console.log(THREE);
 // html에서 캔버스 가져와서 사용
 const canvas = document.querySelector("#three-canvas");
 // const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true, // 계단 현상 없이 부드럽게(성능 저하는 좀 있음)
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Scene
@@ -24,5 +27,20 @@ const camera = new THREE.PerspectiveCamera(
   0.1, // near
   1000 // far
 );
+camera.position.x = 1;
+camera.position.y = 2;
 camera.position.z = 5;
 scene.add(camera);
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  // color: 0xff0000
+  // color: '#ff0000'
+  color: "red",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// 그리기
+renderer.render(scene, camera);
